bba03a082a23c5dbd4375f44fc83bb7be676e67e
ardianilyas <ardianilyas@gmail.com>
2025-06-14 00:10:41 +0700

    update button component

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="bg-[#4640DE] hover:bg-[#332edc] font-medium text-white px-4 py-2 rounded-md cursor-pointer flex gap-3 items-center">
+    <button class="bg-[#4640DE] hover:bg-[#332edc] font-medium text-white px-4 py-2 rounded-md cursor-pointer flex gap-3 items-center justify-center">
         <slot />
     </button>
 </template>
