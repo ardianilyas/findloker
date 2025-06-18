dd9a43e35ad588349d7738ee201cdef56e28e7f9
ardianilyas <ardianilyas@gmail.com>
2025-06-18 07:44:42 +0700

    feat: update login button

---
+++ components/LoginButton.vue
@@ -0,0 +1,9 @@
+<template>
+    <NuxtLink to="/login" class="px-5 py-2 font-medium text-blue-700 border border-blue-700 hover:bg-blue-700 transition-all ease-in-out hover:text-white rounded-md">
+        Login
+    </NuxtLink>
+</template>
+
+<script setup>
+
+</script>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,15 +5,14 @@
             <NuxtLink to="/chats">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-circle-icon lucide-message-circle"><path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z"/></svg>
             </NuxtLink>
-            <NuxtLink to="/">
+            <LoginButton />
+            <!-- <NuxtLink to="/">
                 <div class="w-10 h-10 bg-gray-300 rounded-full flex justify-center items-center">FL</div>
-            </NuxtLink>
+            </NuxtLink> -->
         </div>
     </nav>
 </template>
 
 <script setup lang="ts">
-import { NuxtLink } from '#components';
-
-
+import LoginButton from './LoginButton.vue';
 </script>
